d2d3fe083d2850c3152006e287ad07b396c1f375
Michael Horsley <omega_mike@hotmail.com>
2022-12-14 08:35:27 +0000

    Add a few days worth of templates

---
+++ 2022/Day14.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using BenchmarkDotNet.Attributes;
+using NUnit.Framework;
+
+namespace _2022
+{
+    [TestFixture]
+    public class Day14
+    {
+        [Test]
+        public void PartOne_Test()
+        {
+            var input = FileHelper.GetInputFromFile("Day14_Test.txt");
+
+            Assert.AreEqual(0, Day14Solution.PartOne(input));
+        }
+
+        [Test]
+        public void PartOne_Star()
+        {
+            var input = FileHelper.GetInputFromFile("Day14.txt");
+
+            Assert.AreEqual(0, Day14Solution.PartOne(input));
+        }
+
+        [Test]
+        public void PartTwo_Test()
+        {
+            var input = FileHelper.GetInputFromFile("Day14_Test.txt");
+
+            Assert.AreEqual(0, Day14Solution.PartTwo(input));
+        }
+
+        [Test]
+        public void PartTwo_Star()
+        {
+            var input = FileHelper.GetInputFromFile("Day14.txt");
+
+            Assert.AreEqual(0, Day14Solution.PartTwo(input));
+        }
+    }
+
+    public class Day14Solution
+    {
+        [Benchmark]
+        public static int PartOne(string input)
+        {
+            var rowsList = input.Replace("\r", "").Split("\n").ToList();
+
+            return 0;
+        }
+
+        [Benchmark]
+        public static int PartTwo(string input)
+        {
+            var rowsList = input.Replace("\r", "").Split("\n").ToList();
+
+            return 0;
+        }
+    }
+}
---
+++ 2022/Day16.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using BenchmarkDotNet.Attributes;
+using NUnit.Framework;
+
+namespace _2022
+{
+    [TestFixture]
+    public class Day16
+    {
+        [Test]
+        public void PartOne_Test()
+        {
+            var input = FileHelper.GetInputFromFile("Day16_Test.txt");
+
+            Assert.AreEqual(0, Day16Solution.PartOne(input));
+        }
+
+        [Test]
+        public void PartOne_Star()
+        {
+            var input = FileHelper.GetInputFromFile("Day16.txt");
+
+            Assert.AreEqual(0, Day16Solution.PartOne(input));
+        }
+
+        [Test]
+        public void PartTwo_Test()
+        {
+            var input = FileHelper.GetInputFromFile("Day16_Test.txt");
+
+            Assert.AreEqual(0, Day16Solution.PartTwo(input));
+        }
+
+        [Test]
+        public void PartTwo_Star()
+        {
+            var input = FileHelper.GetInputFromFile("Day16.txt");
+
+            Assert.AreEqual(0, Day16Solution.PartTwo(input));
+        }
+    }
+
+    public class Day16Solution
+    {
+        [Benchmark]
+        public static int PartOne(string input)
+        {
+            var rowsList = input.Replace("\r", "").Split("\n").ToList();
+
+            return 0;
+        }
+
+        [Benchmark]
+        public static int PartTwo(string input)
+        {
+            var rowsList = input.Replace("\r", "").Split("\n").ToList();
+
+            return 0;
+        }
+    }
+}
